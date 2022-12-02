02c7310d7358bf8bd7aca3b143b242898143a384
Brinelson <ladofmusic@gmail.com>
2022-12-01 23:59:49 -0700

    working on save item

---
+++ index.js
@@ -0,0 +1,13 @@
+c724a6eee25e46929d262e94dee683ed
+
+
+const saveNewsItem = (newsItem) => {
+    
+}
+
+
+const printSearchResult = () => {
+    let searchList = document.querySelector("#searchList")
+
+    
+}
