f34dd59e9c4c0fd22428094f2d169a4b82e4bd17
Brinelson <ladofmusic@gmail.com>
2022-12-02 23:59:32 -0700

    create item function test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-c724a6eee25e46929d262e94dee683ed
+// c724a6eee25e46929d262e94dee683ed
 
 
 const saveNewsItem = (newsItem) => {
@@ -6,6 +6,11 @@ const saveNewsItem = (newsItem) => {
 }
 
 
+const createIteam = () => {
+    
+}
+
+console.log('this works')
 const printSearchResult = () => {
     let searchList = document.querySelector("#searchList")
 
