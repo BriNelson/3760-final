76bd13a42de73836dde1f11fd96063fe0ef9b62d
Brinelson <ladofmusic@gmail.com>
2022-12-03 21:46:56 -0700

    bunch of front end work

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 // c724a6eee25e46929d262e94dee683ed
-
+// AIzaSyCloTfnFWI2UH5Q5uuCtW1HbiwdJM22-nQ
 
 const saveNewsItem = (newsItem) => {
     
 }
 
 
-const createIteam = () => {
-    
+async function fetchNews() {
+    const newsResponse = await fetch("https://randomuser.me/api/?gender=male");
+    // console.log(catResponse);
+    const newsRes = await newsResponse.json();
+    console.log(newsRes.results[0].picture.large);
+    return newsRes;
+  }
+
+const getNewsButton = document.querySelector('#callData')
+
+getNewsButton.addEventListener('click', () => {
+    fetchNews()
+})
+
+const createItem = (itemName, elementTag, classOne, classTwo, classThree, appendItem) => {
+
+    itemName = document.createElement(elementTag);
+    itemName.classlist.add(classOne, classTwo, classThree);
+    appendItem.appendChild(itemName);
 }
 
-console.log('this works')
+
 const printSearchResult = () => {
     let searchList = document.querySelector("#searchList")
 
     
-}
+} 
