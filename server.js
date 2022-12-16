5ae4fd5f4afc402e5afecd199964b0f6753ebf56
Brinelson <ladofmusic@gmail.com>
2022-12-15 21:49:01 -0700

    mongo db stuff, need to merge

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://3727137271:4N5usueK6KJz2R@cluster0.olv4a.mongodb.net/?retryWrites=true&w=majority";
+
+const app = express()
 
 mongoose.connect(
     uri,
