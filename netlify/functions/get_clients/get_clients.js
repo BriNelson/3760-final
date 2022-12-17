5044585bbe2b6d1df11160b58617276a78a43634
Brinelson <ladofmusic@gmail.com>
2022-12-17 00:21:39 -0700

    new  post function working

--- netlify/functions/get_clients/get_clients.js
+++ netlify/functions/get_clients/get_clients.js
@@ -1,6 +1,7 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const mongoose = require('mongoose')
 const uri = "mongodb+srv://3727137271:admin@cluster0.olv4a.mongodb.net/?retryWrites=true&w=majority";
+const WorkClientSchema = require('../../../clientModel')
 
 
 mongoose.connect(
@@ -13,33 +14,11 @@ mongoose.connect(
   .catch(console.error)
 
 
-// import pkg from 'mongoose';
-const { model, Schema } = require('mongoose')
 
-
-
-const WorkClientSchema = new Schema({
-    
-    firstName: String,
-    lastName: String,
-    phoneNumber: String,
-    address: String,
-    city: String,
-    state: String,
-    zipCode: Number,
-    contractor: String,
-    notes: String
-
-
-
-})
-
-
-let WorkClient = model("client", WorkClientSchema);
 
 const handler = async (event) => {
  
-  const clients = await WorkClient.find()
+  const clients = await WorkClientSchema.find()
   return {
     statusCode: 200,
     body: JSON.stringify(clients),
